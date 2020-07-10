140d52d24793973456c4a9381b4df5add09ccd94
Sachin Ramteke <sachin06jd@gmail.com>
2020-07-10 10:52:12 -0500

    READ ME file with draft design of UI

---
+++ Untitled Diagram - diagrams.net_files/dropins.js
@@ -0,0 +1,181 @@
+<!-- saved from url=(0294)http://blockedsite.deere.com/ -->
+<!DOCTYPE html PUBLIC "-//W3C//Dtd HTML 4.01 transitional//EN" "http://www.w3c.org/tr/1999/REC-html401-19991224/loose.dtd">
+<html>
+<head>
+<meta content="IE=11.0000" http-equiv="X-UA-Compatible">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"> 
+<LINK href="EndUserNotificationPages.css" type=text/css rel=StyleSheet>
+<title>Caution!</title> 
+
+<script language="JavaScript">
+var aupurl = "http://jdo.deere.com/en-us/corp/compliance/Global_Policies/global_electronic_resources_policy/appendix1_erp_access_use_en_EN.pdf";
+var company = "John Deere";
+var email = "GlobalITServiceDesk@johndeere.com";
+var phone = "1-866-458-0400";
+var defLang='en_US'
+</script>
+ 
+<script type="text/javascript">
+var params = {};
+
+if (location.search) {
+    var parts = location.search.substring(1).split('&');
+
+    for (var i = 0; i < parts.length; i++) {
+        var nv = parts[i].split('=');
+        if (!nv[0]) continue;
+        params[nv[0]] = nv[1] || true;
+    }
+}
+var curl = "http://www.dropbox.com";
+var tempzsq = "DNF0FJD4NH4rDj67n6JrrtLSbnsD61FJMPWNj0r"
+var url = decodeURIComponent(params.url);
+var referer = decodeURIComponent(params.referer);
+var reason = (params.reason).replace(/\+/g, " ");
+var reasoncode = params.reasoncode;
+var timebound = params.timebound;
+var kind = params.kind;
+var rule = params.rule;
+var cat = (params.cat).replace(/\+/g, " ");;
+var user = params.user;
+var zsq = params.zsq;
+var zsq=zsq.substr(0, zsq.length - 3);
+</script>
+ <!-- examples of usage
+<script>document.write(params.url);</script>
+<script>document.write(url);</script>
+<script>document.write(params.referer);</script>
+<script>document.write(referer);</script>
+<script>document.write(params.reason);</script>
+<script>document.write(reason);</script>
+<script>document.write(reasoncode);</script>
+<script>document.write(timebound);</script>
+<script>document.write(kind);</script>
+<script>document.write(rule);</script>
+<script>document.write(params.cat);</script>
+<script>document.write(cat);</script>
+<script>document.write(user);</script>
+<script>document.write(zsq);</script>
+<script>document.write(window.location.href);</script>
+-->
+<style>
+.btn:hover {
+	color: #367c2b;
+	background: #ffffff;
+	}
+.btn {
+    background: #367c2b;
+    color: #ffffff;
+    border-radius: 5px;
+    border: 2px solid #367c2b;
+    cursor: pointer;
+    display: inline-block;
+    height: 30px;
+    margin: 10px 0 15px;
+    font-size: 18px;
+    line-height: 26px;
+    width: auto;
+    padding: 0 20px;
+}
+.f_btn {
+    display: inline-block;
+
+
+</style>
+
+</head>
+<body>
+<table align="center" width="800">
+  
+	<tr>
+        <td class="boxhead" vertical-align=bottom><IMG alt="John Deere" src="jd_logo_desktop.png">
+		<IMG alt="Caution!" align=right src="caution.png">
+		</td>
+	</tr>
+	
+	<tr>
+		<td class="enduser_txt_bold">
+		<br>
+		The site you are requesting (<a href="http://blockedsite.deere.com/%3Cscript%3Edocument.write(url);%3C/script%3E"><script>document.write(url);</script></a>) is not approved for use for Company business.  Use of the site for Company business violates Company policy and subjects the user to discipline and costs.
+		<br>
+		<br>
+		For approved sites for Company business, please see <a href="https://deere.sharepoint.com/sites/ToolFinder/SitePages/ToolsAtoZ.aspx">John Deere’s Tool Finder link</a>.
+		<br>
+		<br>
+		For more information on Caution Pages, visit <a href="https://johndeere.service-now.com/nav_to.do?uri=%2Fkb_view.do%3Fsysparm_article%3DKB0095452%26sysparm_stack%3D%26sysparm_view%3D">KB0095452 - Proxy Caution and Warning pages</a>.
+		<br>
+		<br>
+		Click Continue if you still wish to go to the site for your personal use and assume all related responsibilities.
+		<br>
+		<br>
+		<center>
+		<div class="f_btn">
+			<form method="GET" action="https://gateway.zscloud.net:443/_sm_ctn">
+				<input class="btn" type="submit" value="I understand the risks and want to continue">
+				<script>document.write('<input type=hidden name="_sm_url" value='+url+'');</script>
+				<input type="hidden" name="_sm_url" value="<? echo $url ?>" />
+				<script>document.write('<input type=hidden name="_sm_rid" value='+zsq+'');</script>
+				<input type="hidden" name="_sm_rid" value="<? echo $zsq ?>">
+				<input type="hidden" name="_sm_cat" value="Security+CautionPage">
+			</form> 
+		</center>
+		</div>
+		
+		<!--
+		<div class="f_btn">
+			<form method="GET" action="https://gateway.zscloud.net:443/_sm_ctn">
+				<input class="btn" type="submit" value="Continue">
+				<script>document.write('<input type=hidden name="_sm_url" value='+curl+'');</script>
+				<input type="hidden" name="_sm_url" value="<? echo $curl ?>" />
+				<script>document.write('<input type=hidden name="_sm_rid" value='+tempzsq+'');</script>
+				<input type="hidden" name="_sm_rid" value="<? echo $tempzsq ?>">
+				<input type="hidden" name="_sm_cat" value="Security+CautionPage">
+			</form> 
+		</div>
+		-->
+		
+		<!--
+		<div class="f_btn">
+			<form action='https://gateway.zscloud.net/_sm_ctn?_sm_url=",curl,"&_sm_rid=",sm_rid,"' method='GET'>
+				<input class="btn" type="submit" value="Continue">
+				<script>document.write('<input type=hidden name="_sm_url" value='+curl+'');</script>
+				<input type="hidden" name="_sm_url" value="<? echo $curl ?>" />
+				<script>document.write('<input type=hidden name="_sm_rid" value='+tempzsq+'');</script>
+				<input type="hidden" name="_sm_rid" value="<? echo $tempzsq ?>" />
+				<input type="hidden" name="_sm_cat" value="Security+CautionPage">
+			</form> 
+		</div>
+		-->
+
+		</td><!--locale en_US-->
+	</tr>
+	
+	<tr>
+		<td class="enduser_txt">
+		<br>
+		More information on sites and categories that Deere restricts is available <a href="http://share.deere.com/teams/proxysupport/SitePages/Blocked%20Access.aspx">here.</a>
+		<br>
+		<br>
+		</td>
+	</tr>
+    
+	<tr>
+		<td class="enduser_contact">Read our organization's Internet use policy: <A href="http://jdo.deere.com/en-us/corp/compliance/Global_Policies/global_electronic_resources_policy/appendix1_erp_access_use_en_EN.pdf">
+		Deere Electronic Access Use Policy</A>
+		<br><br>You may contact your IT support administrator at:
+		<br>&nbsp;&nbsp;&nbsp;Phone: <script>document.write(phone);</script>
+		<br>&nbsp;&nbsp;&nbsp;Email: <a href="mailto:GlobalITServiceDesk@johndeere.com?subject=Zscaler%20Blocked%20Page"><script>document.write(email)</script></a>
+		</td>
+	</tr>
+	
+	<tr>
+		<td class="enduser_footer">
+		<br>Accessed by user: <strong><script>document.write(user);</script></strong><br>
+		<p id="time"></p><script>document.getElementById("time").innerHTML = Date();</script>
+		<br><button onclick="window.open('http://ip.zscaler.com/cgi-bin/index.cgi?show=json','debugInfo','width=1000,height=100,scrollbars=yes')">Click for IP information</button>
+		<br><br>
+		</td>
+	</tr>
+</table><!--/locale en_US-->
+</body>
+</html>
